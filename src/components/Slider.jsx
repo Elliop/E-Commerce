d00f3f2dc527a7172472087fbee63d4e1d4413d4
Elliop <abdelmouhayminegx@gmail.com>
2021-10-05 18:59:11 +0100

    Slider Component and Arrows

---
+++ src/components/Slider.jsx
@@ -0,0 +1,47 @@
+import { ArrowLeftOutlined } from "@material-ui/icons";
+import { ArrowRightOutlined } from "@mui/icons-material";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+`;
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Slider = () => {
+  return (
+    <Container>
+      <Arrow direction="left">
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper></Wrapper>
+      <Arrow direction="right">
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default Slider;
