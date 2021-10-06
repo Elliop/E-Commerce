f800bcd0f63dd01c76849c3c2ac5de175611b180
Elliop <abdelmouhayminegx@gmail.com>
2021-10-06 18:56:41 +0100

    Silde

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,12 +1,13 @@
-import { ArrowLeftOutlined } from "@material-ui/icons";
-import { ArrowRightOutlined } from "@mui/icons-material";
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 import styled from "styled-components";
+import { useState } from "react";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   position: relative;
+  overflow: hidden;
 `;
 const Arrow = styled.div`
   width: 50px;
@@ -24,20 +25,72 @@ const Arrow = styled.div`
   margin: auto;
   cursor: pointer;
   opacity: 0.5;
+  z-index: 2;
 `;
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transform: translateX(0vw);
 `;
 
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+const Image = styled.img`
+  height: 80%;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
+const handelClick = (direction) => {};
+
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handelClick("left")}>
         <ArrowLeftOutlined />
       </Arrow>
-      <Wrapper></Wrapper>
-      <Arrow direction="right">
+      <Wrapper>
+        <Slide bg="f5fafd">
+          <ImgContainer>
+            <Image src="https://i.ibb.co/DG69bQ4/2.png" />
+          </ImgContainer>
+          <InfoContainer>
+            <Title>AUTUMN COLLECTION</Title>
+            <Desc>
+              DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.
+            </Desc>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handelClick("right")}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
