630a0a08ee06e5a10249943f8bece8282a9bbfcc
Elliop <abdelmouhayminegx@gmail.com>
2021-10-11 11:13:28 +0100

    Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 const App = () => {
-  return <Login />;
+  return <Cart />;
 };
 
 export default App;
