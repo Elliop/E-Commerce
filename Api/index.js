80e4bd0444ab4c16dee46a80d73afe509ff264a6
Elliop <abdelmouhayminegx@gmail.com>
2021-10-11 19:12:15 +0100

    Auth (ogin && Register)

--- Api/index.js
+++ Api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -16,7 +16,7 @@ mongoose
   });
 
 app.use(express.json());
-app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
