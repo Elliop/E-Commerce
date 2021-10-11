24852c40ea743058ccce7459f345f0c91e14e201
Elliop <abdelmouhayminegx@gmail.com>
2021-10-11 11:46:59 +0100

    Responsive Newslletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,5 +1,6 @@
 import { Send } from "@material-ui/icons";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60vh;
@@ -13,11 +14,14 @@ const Title = styled.h1`
   font-size: 70px;
   margin-bottom: 20px;
 `;
-const Description = styled.div`
-  font-size: 34px;
+
+const Desc = styled.div`
+  font-size: 24px;
   font-weight: 300;
   margin-bottom: 20px;
+  ${mobile({ textAlign: "center" })}
 `;
+
 const InputContainer = styled.div`
   width: 50%;
   height: 40px;
@@ -25,25 +29,27 @@ const InputContainer = styled.div`
   display: flex;
   justify-content: space-between;
   border: 1px solid lightgray;
+  ${mobile({ width: "80%" })}
 `;
+
 const Input = styled.input`
   border: none;
   flex: 8;
   padding-left: 20px;
 `;
+
 const Button = styled.button`
   flex: 1;
   border: none;
   background-color: teal;
   color: white;
-  cursor: pointer;
 `;
 
 const Newsletter = () => {
   return (
     <Container>
       <Title>Newsletter</Title>
-      <Description>Get timely updates from your favorite products.</Description>
+      <Desc>Get timely updates from your favorite products.</Desc>
       <InputContainer>
         <Input placeholder="Your email" />
         <Button>
