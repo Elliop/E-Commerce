5d0e846095c85e57d75c7a1e1c53383fad7bd5cf
Elliop <abdelmouhayminegx@gmail.com>
2021-10-11 12:03:22 +0100

    Responsive Cart

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -87,7 +87,7 @@ const Footer = () => {
   return (
     <Container>
       <Left>
-        <Logo>LAMA.</Logo>
+        <Logo>ELIA.</Logo>
         <Desc>
           There are many variations of passages of Lorem Ipsum available, but
           the majority have suffered alteration in some form, by injected
