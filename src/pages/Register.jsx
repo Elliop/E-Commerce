da3915c6fa5662c242e60930651008808710ca76
Elliop <abdelmouhayminegx@gmail.com>
2021-10-11 11:59:17 +0100

    Responsive Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   width: 100vw;
@@ -14,29 +15,36 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
 const Wrapper = styled.div`
   width: 40%;
   padding: 20px;
   background-color: white;
+  ${mobile({ width: "75%" })}
 `;
+
 const Title = styled.h1`
   font-size: 24px;
   font-weight: 300;
 `;
+
 const Form = styled.form`
   display: flex;
   flex-wrap: wrap;
 `;
+
 const Input = styled.input`
   flex: 1;
   min-width: 40%;
-  margin: 20px 10px 0 0;
+  margin: 20px 10px 0px 0px;
   padding: 10px;
 `;
+
 const Agreement = styled.span`
   font-size: 12px;
-  margin: 20px 0;
+  margin: 20px 0px;
 `;
+
 const Button = styled.button`
   width: 40%;
   border: none;
