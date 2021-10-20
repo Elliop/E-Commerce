fa2fa82ced1499c4144121153f54da0bd50feed6
Elliop <abdelmouhayminegx@gmail.com>
2021-10-20 10:14:44 +0100

    Update user infos

--- Api/index.js
+++ Api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ mongoose
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
--- Api/routes/verifyToken.js
+++ Api/routes/verifyToken.js
@@ -14,4 +14,14 @@ const verifyToken = (req, res, next) => {
   }
 };
 
-module.exports = { verifyToken };
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are not alowed to do that!");
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyTokenAndAuthorization };
