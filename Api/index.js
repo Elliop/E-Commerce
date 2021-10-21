520c9beb75a19561b04cef53c28deb08b6f726b1
Elliop <abdelmouhayminegx@gmail.com>
2021-10-21 13:41:09 +0100

    Add stripe route in index

--- Api/index.js
+++ Api/index.js
@@ -7,14 +7,13 @@ const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("DB Connection Successfull!");
-  })
+  .then(() => console.log("DB Connection Successfull!"))
   .catch((err) => {
     console.log(err);
   });
@@ -25,6 +24,7 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
